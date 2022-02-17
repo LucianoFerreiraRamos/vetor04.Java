5cedc43363f83e54de5520409535e72840354202
Luciano <97047885+LucianoFerreiraRamos@users.noreply.github.com>
2022-02-17 11:21:51 -0300

    Vetor04

---
+++ src/vetor04/Vetor04.java
@@ -0,0 +1,21 @@
+package vetor04;
+
+import java.util.Arrays;
+
+public class Vetor04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int vet[] = {3,7,6,1,9,4,2};
+		for(int v : vet) {
+			System.out.print(v + " ");
+			
+		}
+		System.out.println("   ");
+		int p = Arrays.binarySearch(vet, 1);
+		System.out.println("O valor encontrado na posição solicitada foi  : " + p );
+		
+
+	}
+
+}
